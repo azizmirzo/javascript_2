85aae8c1160b4a0e388a54cbcc520c454ff67c3e
Abdulaziz <93951510+azizmirzo@users.noreply.github.com>
2022-04-22 20:40:55 +0500

    Update script.js

--- dastur/script.js
+++ dastur/script.js
@@ -1,13 +1,13 @@
-window.addEventListener(type:'DOMContentLoaded',listener: function() {
-  let products = document.querySelectorAll(selectors:'.product'),
-      buttons = document.querySelectorAll(selectors:'button'),
-      openBtn = document.querySelector(selectors:'.open');
+window.addEventListener('DOMContentLoaded', function() {
+  let products = document.querySelectorAll('.product'),
+      buttons = document.querySelectorAll('button'),
+      openBtn = document.querySelector('.open');
 
   function createCart() {
-     let cart = document.createElement(tagName:'div'),
-         field = document.createElement(tagName:'div'),
-         heading = document.createElement(tagName:'h2'),
-         closeBtn = document.createElement(tagName:'button');
+     let cart = document.createElement('div'),
+         field = document.createElement('div'),
+         heading = document.createElement('h2'),
+         closeBtn = document.createElement('button');
      
      cart.classList.add('cart')
      field.classList.add('cart-field')
@@ -23,21 +23,21 @@ window.addEventListener(type:'DOMContentLoaded',listener: function() {
   } 
   createCart()
 
-  let cart = document.querySelector(selectors: '.cart'),
-      closeBtn = document.querySelector(selectors:'.close'),
-      field = document.querySelector(selectors:'.cart-fiald')
+  let cart = document.querySelector('.cart'),
+      closeBtn = document.querySelector('.close'),
+      field = document.querySelector('.cart-fiald')
 
-  openBtn.addEventListener(type: 'click', listener: function () {
+  openBtn.addEventListener( 'click', function () {
      cart.style.display = 'block'
   })
-  closeBtn.addEventListener(type:'click', listener:function () {
+  closeBtn.addEventListener('click',function () {
      cart.style.display = 'none'
   })
 
-  buttons.forEach(callbacfn:function(item :HTMLButtonElement, i :number) {
-    item.addEventListener(type:'click', listener: function () {
-      let cloneItem = products[i].cloneNode(deep: true),
-         btn = cloneItem.querySelector(selectors: 'button');
+  buttons.forEach(function(item :HTMLButtonElement, i :number) {
+    item.addEventListener('click', function () {
+      let cloneItem = products[i].cloneNode(true),
+         btn = cloneItem.querySelector('button');
 
       btn.remove()
       field.appendChild(cloneItem)
